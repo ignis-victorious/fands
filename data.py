2616d6d025c089348f6c9c19719cd0b5adfb4ec7
Emmanuele Levi <ignisvictorious@gmail.com>
2025-03-31 15:11:05 +0100

    Path parameters and type hint

---
+++ data.py
@@ -0,0 +1,11 @@
+#  __________________
+#  Import LIBRARIES
+#  Import FILES
+#  __________________
+
+BANDS: list[dict] = [
+    {"id": 1, "name": "The Kinks", "genre": "Rock"},
+    {"id": 2, "name": "Aphex Twin", "genre": "Electronic"},
+    {"id": 3, "name": "Slowdive", "genre": "Shoegaze"},
+    {"id": 4, "name": "Wu-Tang Clan", "genre": "Hip-Hop"},
+]
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 #  __________________
 #  Import LIBRARIES
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from enum import Enum
+
 #  Import FILES
+from data import BANDS
 #  __________________
+
+
+class GenreURLChoices(Enum):
+    ROCK = "rock"
+    ELECTRONIC = "electronic"
+    METAL = "metal"
+    HIP_HOP = "hip-hop"
 
 
 app = FastAPI()
@@ -13,13 +23,31 @@ async def index() -> dict[str, str]:
     return {"This is": "root"}
 
 
-@app.get("/about")
-async def about() -> str:
-    return "What a lovely day"
+@app.get("/bands")
+async def bands() -> list[dict]:
+    return BANDS
+
+
+@app.get("/bands/{band_id}")
+# @app.get("/bands/{band_id}", status_code=206)
+async def band(band_id: int) -> dict | None:
+    band: dict | None = next((b for b in BANDS if b["id"] == band_id), None)
+    if band is None:
+        #  Status code 404
+        raise HTTPException(status_code=404, detail="Band not found!")
+    return band
+
+
+@app.get("/bands/genre/{genre}")
+async def bands_for_genre(genre: GenreURLChoices) -> list[dict]:
+    # async def bands_for_genre(genre: str) -> list[dict]:
+    return [b for b in BANDS if b["genre"].lower() == genre.value]
+    # return [b for b in BANDS if b["genre"].lower() == genre.lower()]
 
 
 # def main():
 #     print("Hello from fands!")
+
 # if __name__ == "__main__":
 #     main()
 
