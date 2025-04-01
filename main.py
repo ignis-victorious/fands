7ffe2bab95904d9f42da5953b98d5dd3a491b60c
Emmanuele Levi <ignisvictorious@gmail.com>
2025-04-01 23:37:21 +0100

    Post request, little bug due to pydantic 2

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #  __________________
 #  Import LIBRARIES
+from typing import Any
 from fastapi import FastAPI, HTTPException
 
 #  Import FILES
 from data import BANDS
-from schemas import GenreURLChoices, Band
+from schemas import GenreURLChoices, BandBase, BandCreate, BandWithID
 #  __________________
 
 
@@ -19,11 +20,12 @@ async def index() -> dict[str, str]:
 @app.get("/bands")
 async def bands(
     genre: GenreURLChoices | None = None, has_albums: bool = False
-) -> list[Band]:
-    band_list: list[Band] = [Band(**b) for b in BANDS]
+) -> list[BandWithID]:
+    band_list: list[BandWithID] = [BandWithID(**b) for b in BANDS]
 
     if genre:
-        band_list = [b for b in band_list if b.genre.lower() == genre.value]
+        band_list = [b for b in band_list if b.genre.value.lower() == genre.value]
+        # band_list = [b for b in band_list if b.genre.lower() == genre.value]
         # return [Band(**b) for b in BANDS if b["genre"].lower() == genre.value]
     if has_albums:
         band_list = [b for b in band_list if len(b.albums) > 0]
@@ -31,11 +33,21 @@ async def bands(
 
 
 @app.get("/bands/{band_id}")
-async def band(band_id: int) -> Band:
-    band: Band | None = next((Band(**b) for b in BANDS if b["id"] == band_id), None)
+async def band(band_id: int) -> BandWithID:
+    band: BandWithID | None = next(
+        (BandWithID(**b) for b in BANDS if b["id"] == band_id), None
+    )
     if band is None:
         #  Sttus code 404
         raise HTTPException(status_code=404, detail="Band not found!")
+    return band
+
+
+@app.post("/bands")
+async def create_band(band_data: BandCreate) -> dict[str, Any]:
+    id: int = BANDS[-1]["id"] + 1
+    band: dict[str, Any] = BandWithID(id=id, **band_data.model_dump()).model_dump()
+    BANDS.append(band)
     return band
 
 
--- schemas.py
+++ schemas.py
@@ -1,6 +1,6 @@
 #  __________________
 #  Import LIBRARIES
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from datetime import date
 from enum import Enum
 #  Import FILES
@@ -19,14 +19,49 @@ class GenreURLChoices(Enum):
     HIP_HOP = "hip-hop"
 
 
+class GenreChoices(Enum):
+    ROCK = "Rock"
+    ELECTRONIC = "Electronic"
+    METAL = "Metal"
+    HIP_HOP = "Hip-hop"
+
+
 class Album(BaseModel):
     title: str
     release_date: date
 
 
-class Band(BaseModel):
-    #  "id": 1, "name": "The Kinks", "genre": "Rock"
-    id: int
+class BandBase(BaseModel):
     name: str
-    genre: str
+    genre: GenreChoices
     albums: list[Album] = []
+
+
+#  - OLD -
+# class BandBase(BaseModel):
+#     name: str
+#     genre: str
+#     albums: list[Album] = []
+
+
+# class Band(BaseModel):
+#     #  "id": 1, "name": "The Kinks", "genre": "Rock"
+#     id: int
+#     name: str
+#     genre: str
+#     albums: list[Album] = []
+
+
+class BandCreate(BandBase):
+    @field_validator("genre", mode="before")
+    # @classmethod
+    def title_case_genre(cls, value) -> str:
+        return value.title()
+
+
+# class BandCreate(BandBase):
+#     pass
+
+
+class BandWithID(BandBase):
+    id: int
